f1f19167226043d788d773b5e9a6ad23e8d95519
MentalSep <chahidjava@hotmail.com>
2024-02-11 08:23:01 +0100

    add task 2: 	reminder to use localhost for testing

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  const checkedAmenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checkedAmenities[$(this).data('id')];
+    }
+    $('div.amenities h4').text(Object.values(checkedAmenities).join(', '));
+  });
+  // use localhost for testing if not working because of CORS
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    }
+  });
+});
